539b686322efce0d1ee687a6a6bde6102d17ef8b
Holdypopn <dominik.n8@googlemail.com>
2023-02-28 21:16:15 +0100

    Add TMPro + EnemyHandling + EnemyKilledEvent

--- the_game/Assets/Scripts/Bullet.cs
+++ the_game/Assets/Scripts/Bullet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float damage = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,11 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if(collision.gameObject.TryGetComponent<Enemy>(out Enemy enemyComponent))
+        {
+            enemyComponent.TakeDamage(damage);
+        }
+
         Destroy(gameObject);
     }
 }
---
+++ the_game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI enemiesLeftText;
+    List<Enemy> enemies = new List<Enemy>();
+
+    private void OnEnable()
+    {
+        Enemy.OnEnemyKilled += HandleEnemyDefeated;
+    }
+
+    private void OnDisable()
+    {
+        Enemy.OnEnemyKilled -= HandleEnemyDefeated;
+    }
+
+    private void Awake()
+    {
+        enemies = GameObject.FindObjectsOfType<Enemy>().ToList();
+        UpdateEnemiesLeftText();
+    }
+
+    void HandleEnemyDefeated(Enemy enemy)
+    {
+        if(enemies.Remove(enemy))
+            UpdateEnemiesLeftText();
+    }
+
+    void UpdateEnemiesLeftText()
+    {
+        enemiesLeftText.text = $"Enemies Left: {enemies.Count}";
+    }
+}
