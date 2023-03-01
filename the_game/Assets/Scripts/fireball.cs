31e5accaa69dc53d0bf9c397b24a7aa1416cfe7b
AsApp9 <peter.rehders@gmx.de>
2023-03-01 21:46:16 +0100

    fireball animation

--- the_game/Assets/Scripts/fireball.cs
+++ the_game/Assets/Scripts/fireball.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class fireball : MonoBehaviour
 {
     public float damage = 1;
+    private Animator anim;
 
     // Start is called before the first frame update
+    void OnBecameInvisible() 
+    {
+         Destroy(gameObject);
+    }
+   
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,13 +24,15 @@ public class Bullet : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
+        
         if(collision.gameObject.TryGetComponent<Enemy>(out Enemy enemyComponent))
         {
             enemyComponent.TakeDamage(damage);
+            anim.SetTrigger("onDeath");
+            
+            Destroy(gameObject, 0.3f);
         }
-
-        Destroy(gameObject);
     }
 }
