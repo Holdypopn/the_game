110dc213c70e5a941135cde8d557088963dd2477
Holdypopn <dominik.n8@googlemail.com>
2023-03-03 21:51:58 +0100

    Add dash

--- the_game/Assets/Scripts/PlayerController.cs
+++ the_game/Assets/Scripts/PlayerController.cs
@@ -8,45 +8,83 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     public Weapon weapon;
 
+    private enum State
+    {
+        Normal,
+        Dashing
+    }
+
+    private State state;
     Vector2 moveDirection;
     Vector2 mousePosition;
+    private float dashSpeed;
+    private Vector2 dashDirection;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = State.Normal;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        
-
-        moveDirection = new Vector2(moveX, moveY).normalized;
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    }
-
-    void LateUpdate()
-    {
-        if(Input.GetMouseButtonDown(0))
+        switch(state)
         {
-            weapon.ShootFireball();
-        }
-        if(Input.GetMouseButtonDown(1))
-        {
-            weapon.ShootBlueFireball();
+            case State.Normal:
+                float moveX = Input.GetAxisRaw("Horizontal");
+                float moveY = Input.GetAxisRaw("Vertical");
+
+                
+                if(Input.GetMouseButtonDown(0))
+                {
+                    weapon.ShootFireball();
+                }
+                if(Input.GetMouseButtonDown(1))
+                {
+                    weapon.ShootBlueFireball();
+                }
+
+                moveDirection = new Vector2(moveX, moveY).normalized;
+                mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+                //Dash
+                if(Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    dashDirection = moveDirection;
+                    dashSpeed = 80f;
+                    state = State.Dashing;
+                }
+                break;
+
+            case State.Dashing:
+                float dashSpeedDropMultiplier = 5f;
+                dashSpeed -= dashSpeed * dashSpeedDropMultiplier * Time.deltaTime;
+
+                float dashSpeedMinimum = 50f;
+                if(dashSpeed < dashSpeedMinimum)
+                {
+                    state = State.Normal;
+                }
+                break;
         }
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        switch(state)
+        {
+            case State.Normal:
+                rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
 
-        Vector2 aimDirection = mousePosition - rb.position;
-        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = aimAngle;
+                Vector2 aimDirection = mousePosition - rb.position;
+                float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+                rb.rotation = aimAngle;
+                break;
+            case State.Dashing:
+                rb.velocity = dashDirection * dashSpeed;
+                break;
+        }
     }
 }
