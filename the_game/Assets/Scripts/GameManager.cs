b9a0b794890096fe506f1c7e04079b196ed5cac8
Holdypopn <dominik.n8@googlemail.com>
2023-03-03 19:48:52 +0100

    EnemySpawner

--- the_game/Assets/Scripts/GameManager.cs
+++ the_game/Assets/Scripts/GameManager.cs
@@ -8,21 +8,30 @@ public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI enemiesLeftText;
     List<Enemy> enemies = new List<Enemy>();
+    public GameObject enemyToSpawn;
 
     private void OnEnable()
     {
         Enemy.OnEnemyKilled += HandleEnemyDefeated;
+        Enemy.OnEnemyKilled += SpawnNewEnemy;
     }
 
     private void OnDisable()
     {
         Enemy.OnEnemyKilled -= HandleEnemyDefeated;
+        Enemy.OnEnemyKilled -= SpawnNewEnemy;
     }
 
     private void Awake()
     {
         enemies = GameObject.FindObjectsOfType<Enemy>().ToList();
         UpdateEnemiesLeftText();
+    }
+
+    void SpawnNewEnemy(Enemy enemy)
+    {
+        Vector2 randomPositionOnScreen = Camera.main.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
+        Instantiate(enemyToSpawn, randomPositionOnScreen, Quaternion.identity);
     }
 
     void HandleEnemyDefeated(Enemy enemy)
