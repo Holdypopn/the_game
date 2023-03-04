ea5b037cf79bf936bc04d601d8db603466d19b66
Holdypopn <dominik.n8@googlemail.com>
2023-03-04 20:04:31 +0100

    Enemy can attack + Health and Shield bar

--- the_game/Assets/Scripts/Enemy.cs
+++ the_game/Assets/Scripts/Enemy.cs
@@ -8,9 +8,13 @@ public class Enemy : MonoBehaviour
     public static event Action<Enemy> OnEnemyKilled;
     public float health, maxHealth = 3f;
     public float moveSpeed = 10;
+    public float damage = 1;
+    public float enemyCoolDown = 2;
 
     private GameObject target;
     private Animator anim;
+    private bool playerInRange = false;
+    private bool canAttack = true;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +28,31 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+
+        if(playerInRange && canAttack)
+        {
+            GameObject.FindWithTag("Player").GetComponent<Player>().currentHealth -= damage;
+            StartCoroutine(AttackCoolDown());
+        }
+    }
+
+    IEnumerator AttackCoolDown()
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(enemyCoolDown);
+        canAttack = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+            playerInRange = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+            playerInRange = false;
     }
 
     public void TakeDamage(float damageAmount)
