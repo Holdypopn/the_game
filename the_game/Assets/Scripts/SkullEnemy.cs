054928e5938f9556485a3040df1ad238d1c9b7a2
Holdypopn <dominik.n8@googlemail.com>
2023-03-29 10:40:10 +0200

    Refactor Skull behavior

--- the_game/Assets/Scripts/SkullEnemy.cs
+++ the_game/Assets/Scripts/SkullEnemy.cs
@@ -13,6 +13,8 @@ public class SkullEnemy : Enemy
     public float maxDistance = 15f;
     public GameObject projectile;
     public float startTimeBetweenShots;
+    public Transform viewPoint;
+    public float viewDistance = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -30,25 +32,22 @@ public class SkullEnemy : Enemy
     // Update is called once per frame
     void Update()
     {
-        if(GetRangeToPlayer(player) > maxDistance)
+        //Debug.Log(CanSeePlayer(viewDistance));
+
+        // wenn inRange aber nicht canSee dann moveCloser
+
+        if(GetRangeToPlayer(player) > maxDistance || !CanSeePlayer())
         {
             agent.isStopped = false;
             agent.SetDestination(player.transform.position);
             //transform.position = Vector2.MoveTowards(transform.position, player.transform.position, this.moveSpeed * Time.deltaTime);
         }
 
-        if(GetRangeToPlayer(player) <= maxDistance / 3)
-        {
-            agent.isStopped = false;
-            agent.SetDestination(player.transform.position);
-            //transform.position = Vector2.MoveTowards(transform.position, player.transform.position, this.moveSpeed * Time.deltaTime);
-        }
-
-        if(GetRangeToPlayer(player) < maxDistance && GetRangeToPlayer(player) > maxDistance / 3)
+        if(GetRangeToPlayer(player) < maxDistance && CanSeePlayer())
             agent.isStopped = true;
 
 
-        if(timeBetweenShoots <= 0)
+        if(timeBetweenShoots <= 0 && CanSeePlayer())
         {
             Instantiate(projectile, transform.position, Quaternion.identity);
             timeBetweenShoots = startTimeBetweenShots;
@@ -62,6 +61,27 @@ public class SkullEnemy : Enemy
     float GetRangeToPlayer(GameObject player)
     {
         return Vector2.Distance(transform.position, player.transform.position);
+    }
+
+    bool CanSeePlayer()
+    {
+        bool canSeePlayer = false;
+
+        RaycastHit2D hit = Physics2D.Linecast(viewPoint.position, player.transform.position, 1 << LayerMask.NameToLayer("Action"));
+
+        if(hit.collider != null)
+        {
+            if(hit.collider.gameObject.CompareTag("Player"))
+                canSeePlayer = true;
+            else
+                canSeePlayer = false;
+
+            Debug.DrawLine(viewPoint.position, hit.point, Color.red);
+        }
+        else
+            Debug.DrawLine(viewPoint.position, hit.point, Color.yellow);
+
+        return canSeePlayer;
     }
 
     void OnCollisionEnter2D(Collision2D col)
